e88ae4fcfbca964e9d95e57e7f7245b795f8638a
ritikkaliyar-ai <ritikkaliyar13@gmail.com>
2026-04-09 19:22:32 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title='sales analysis',page_icon=":bar_chart:",layout="wide")
 st.title(" :bar_chart: sample superstore analysis")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>',unsafe_allow_html=True)
 
 # fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
 # if fl is not None:
